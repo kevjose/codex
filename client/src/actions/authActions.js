ec086743c06f0671bece68cbe77f82b2b355e2b8
Kevin Jose <kevjose@Kevins-MacBook-Pro.local>
2019-06-09 09:27:56 +0530

    changes in base uri react app

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -5,7 +5,10 @@ import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from './types';
 // Register User
 export const registerUser = (userData, history) => dispatch => {
   axios
-    .post(window.location.hostname + ':5000/api/users/register', userData)
+    .post(
+      '//' + window.location.hostname + ':5000/api/users/register',
+      userData
+    )
     .then(res => history.push('/login')) // re-direct to login on successful register
     .catch(err =>
       dispatch({
@@ -17,7 +20,7 @@ export const registerUser = (userData, history) => dispatch => {
 // Login - get user token
 export const loginUser = userData => dispatch => {
   axios
-    .post(window.location.hostname + ':5000/api/users/login', userData)
+    .post('//' + window.location.hostname + ':5000/api/users/login', userData)
     .then(res => {
       // Save to localStorage
       // Set token to localStorage
